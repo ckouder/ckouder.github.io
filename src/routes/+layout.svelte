44896fcabbb21f1d86168a106025f4e08bdab129
Bingji Guo <guobingji@126.com>
2023-07-10 20:21:47 -0500

    add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import Deck from '$lib/components/apps/Deck/Deck.svelte';
 	import Toolbar from '$lib/components/apps/Menubar/Menubar.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+ 
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main>
